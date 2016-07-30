3bd80938be5a7b1210145f2c2290637e42b3ec6b
Ray <ray@rayps.com>
2016-07-30 22:23:29 +0800

    Initial Commit

---
+++ Spring Visualizer App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spring Visualizer App
+//
+//  Created by Ray on 7/30/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
