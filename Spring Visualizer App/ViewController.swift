8d946d2703f3002b9106d62002f13f199132241a
Ray <ray@rayps.com>
2016-07-31 01:33:31 +0800

    WKWebView

--- Spring Visualizer App/ViewController.swift
+++ Spring Visualizer App/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    override func prefersStatusBarHidden() -> Bool { return true }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let webView = WKWebView(frame: view.frame)
+        view.addSubview(webView)
+        webView.backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 0.12, alpha: 1)
+        
+        let path = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "HTML")!
+        let url = NSURL(fileURLWithPath: path)
+        
+        webView.loadFileURL(url, allowingReadAccessToURL: url)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
