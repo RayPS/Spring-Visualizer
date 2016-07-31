5b033c4b3f77a21d11931e1a8cae0a2b3e0108db
Ray <ray@rayps.com>
2016-07-31 09:07:52 +0800

    Rename files and folders for creating Mac version

---
+++ Spring Visualizer Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spring Visualizer
+//
+//  Created by Ray on 7/31/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spring Visualizer Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spring Visualizer
+//
+//  Created by Ray on 7/31/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
