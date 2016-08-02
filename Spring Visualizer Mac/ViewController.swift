05ddcd307c0a217db6bc1986b493cca7c8c677e1
Ray <ray@rayps.com>
2016-08-03 01:31:00 +0800

    No title bar

--- Spring Visualizer Mac/ViewController.swift
+++ Spring Visualizer Mac/ViewController.swift
@@ -7,21 +7,35 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        view.layer!.backgroundColor = NSColor(hue: 0, saturation: 0, brightness: 0.1, alpha: 1).CGColor
+        
+        let webView = WKWebView(frame: view.frame)
+        view.addSubview(webView)
+        
+        
+        let path = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "HTML")!
+        let url = NSURL(fileURLWithPath: path)
+        
+        webView.setValue(true, forKey: "drawsTransparentBackground")
+        webView.loadFileURL(url, allowingReadAccessToURL: url)
+    }
+    
+    
 }
 
---
+++ Spring Visualizer Mac/WindowController.swift
@@ -0,0 +1,21 @@
+//
+//  WindowController.swift
+//  Spring Visualizer App
+//
+//  Created by Ray on 7/31/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        self.window?.titlebarAppearsTransparent = true
+    }
+
+}
